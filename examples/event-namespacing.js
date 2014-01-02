9166a57614511804777530207f69df28da037ae7
Damon Oehlman <damon.oehlman@gmail.com>
2014-01-02 13:03:26 +1100

    Tweak examples

--- examples/event-namespacing.js
+++ examples/event-namespacing.js
@@ -9,3 +9,18 @@ glue.events.once('ready', function() {
   // will also be triggered once ready, and equivalent to glue.ready
   // when directly using eve
 });
+
+// listen for connected events
+// NOTE: only trigger when a page has valid peer elements
+glue.events.once('connected', function(signaller) {
+	console.log('connected');
+
+	signaller.on('color', function(data) {
+		console.log('received color notification: ', arguments);
+	});
+
+	signaller.send('/color', {
+		src: signaller.id,
+		color: 'blue'
+	});
+});
